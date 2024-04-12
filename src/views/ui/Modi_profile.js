bf592f4431495febea9e5acb852d3eabc2c1919a
yseo14 <lmslys14@naver.com>
2024-04-12 19:31:30 +0900

    DTO-193 프로필 수정 form 구현(프로필 사진 추가해야함)

--- src/views/ui/Modi_profile.js
+++ src/views/ui/Modi_profile.js
@@ -1,206 +1,171 @@
 import React, { useState } from "react";
-import {
-  Alert,
-  UncontrolledAlert,
-  Card,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Helmet } from "react-helmet";
+import Form from "react-bootstrap/Form";
 
-const Modi_profile = () => {
-  // For Dismiss Button with Alert
-  const [visible, setVisible] = useState(true);
+import "./Modi_profile.css";
 
-  const onDismiss = () => {
-    setVisible(false);
+const Modi_profile = (props) => {
+  const [nickname, setNickname] = useState(""); // 초기값을 빈 문자열로 설정합니다.
+  const [onelineIntro, setOnelineIntro] = useState(""); // 초기값을 빈 문자열로 설정합니다.
+  const [password, setPassword] = useState(""); // 초기값을 빈 문자열로 설정합니다.
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "nickname":
+        setNickname(value);
+        break;
+      case "onelineIntro":
+        setOnelineIntro(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "passwordConfirmation":
+        setPasswordConfirmation(value);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
-    <div>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-1*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2"> </i>
-          Alert
-        </CardTitle>
-        <CardBody className="">
-          <div className="mt-3">
-            <Alert color="primary">
-              This is a primary alert— check it out!
-            </Alert>
-            <Alert color="secondary">
-              This is a secondary alert— check it out!
-            </Alert>
-            <Alert color="success">
-              This is a success alert— check it out!
-            </Alert>
-            <Alert color="danger">This is a danger alert— check it out!</Alert>
-            <Alert color="warning">
-              This is a warning alert— check it out!
-            </Alert>
-            <Alert color="info">This is a info alert— check it out!</Alert>
-            <Alert color="light">This is a light alert— check it out!</Alert>
-            <Alert color="dark">This is a dark alert</Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-2*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Links
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert color="primary">
-              This is a primary alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="secondary">
-              This is a secondary alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="success">
-              This is a success alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="danger">
-              This is a danger alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="warning">
-              This is a warning alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="info">
-              This is a info alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="light">
-              This is a light alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-            <Alert color="dark">
-              This is a dark alert with
-              <a href="/" className="alert-link">
-                an example link
-              </a>
-              . Give it a click if you like.
-            </Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-3*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Additional content
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert color="success">
-              <h4 className="alert-heading">Well done!</h4>
-              <p>
-                Aww yeah, you successfully read this important alert message.
-                This example text is going to run a bit longer so that you can
-                see how spacing within an alert works with this kind of content.
-              </p>
-              <hr />
-              <p className="mb-0">
-                Whenever you need to, be sure to use margin utilities to keep
-                things nice and tidy.
-              </p>
-            </Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-4*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Dissmissing
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert color="info" isOpen={visible} toggle={onDismiss.bind(null)}>
-              I am an alert and I can be dismissed!
-            </Alert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-5*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alert with Uncontrolled [disable] Alerts
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <UncontrolledAlert color="info">
-              I am an alert and I can be dismissed!
-            </UncontrolledAlert>
-          </div>
-        </CardBody>
-      </Card>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-6*/}
-      {/* --------------------------------------------------------------------------------*/}
-      <Card>
-        <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-          <i className="bi bi-bell me-2" />
-          Alerts without fade
-        </CardTitle>
-        <CardBody className="">
-          <div>
-            <Alert
-              color="primary"
-              isOpen={visible}
-              toggle={onDismiss.bind(null)}
-              fade={false}
-            >
-              I am a primary alert and I can be dismissed without animating!
-            </Alert>
-            <UncontrolledAlert color="warning" fade={false}>
-              I am an alert and I can be dismissed without animating!
-            </UncontrolledAlert>
-          </div>
-        </CardBody>
-      </Card>
+    <div className="Modi-profile-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="Modi-profile">
+        <div className="title-div">
+          <span className="title-text">
+            <span>프로필 수정</span>
+          </span>
+        </div>
 
-      {/* --------------------------------------------------------------------------------*/}
-      {/* End Inner Div*/}
-      {/* --------------------------------------------------------------------------------*/}
+        <form action="/profile-edit" method="post">
+          <div className="nickname-edit">
+            <input
+              id="nickname"
+              name="nickname"
+              type="text"
+              className="edit-value"
+              value={nickname}
+              onChange={handleChange}
+              placeholder="닉네임을 입력하세요."
+            ></input>
+            <span className="edit-element">
+              <span>닉네임 변경</span>
+            </span>
+          </div>
+          <div className="one-line-edit">
+            <input
+              id="onelineIntro"
+              name="onelineIntro"
+              type="text"
+              className="edit-value"
+              value={onelineIntro}
+              onChange={handleChange}
+              placeholder="한줄소개를 입력하세요."
+            ></input>
+            <span className="edit-element">
+              <span>한줄소개 변경</span>
+            </span>
+          </div>
+          <div className="password-edit">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="edit-value"
+              value={password}
+              onChange={handleChange}
+              placeholder="비밀번호를 입력하세요."
+            ></input>
+            <span className="edit-element">
+              <span>비밀번호 변경</span>
+            </span>
+          </div>
+          <div className="passwordConfirmation">
+            <input
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+              type="password"
+              className="edit-value"
+              value={passwordConfirmation}
+              onChange={handleChange}
+              placeholder="비밀번호를 다시 입력하세요."
+            ></input>
+            <span className="edit-element">
+              <span>비밀번호 확인</span>
+            </span>
+          </div>
+          <div className="agreement">
+            <div className="agreement-check">
+              <Form.Check // prettier-ignore
+                type="switch"
+                id="custom-switch"
+              />
+            </div>
+
+            <span className="edit-element">
+              <span>마케팅 수신동의</span>
+            </span>
+          </div>
+          <div className="group95-frame5">
+            <span className="group95-text20">
+              <span>중복확인</span>
+            </span>
+          </div>
+          <div className="group95-frame6">
+            <span className="group95-text22">
+              <span>
+                ・ 최소 8자 이상, 대문자, 소문자, 숫자 및 기호의 혼합 포함
+              </span>
+            </span>
+            <span className="group95-text24">
+              <span>・ 기호(예: !”$%^*&amp;)</span>
+            </span>
+            <span className="group95-text26">
+              <span>・ 대문자(A~Z), 소문자(a~z)</span>
+            </span>
+          </div>
+
+          {/* <span className="group95-text32">
+            <span>동의</span>
+          </span>
+          <span className="group95-text34">
+            <span>미동의</span>
+          </span> */}
+        </form>
+        <div className="group95-frame7">
+          <span className="group95-text28">
+            <span>수정하기</span>
+          </span>
+        </div>
+        <div className="group95-frame8">
+          <span className="group95-text30">
+            <span>닫기</span>
+          </span>
+        </div>
+
+        {/* <div className="group95-checked-circle">
+          <img
+            src="/external/image9512-2mgi-200h.png"
+            alt="IMAGE9512"
+            className="group95-image3"
+          />
+          <img
+            src="/external/image9512-ozr-200h.png"
+            alt="IMAGE9512"
+            className="group95-image4"
+          />
+        </div>
+        <img
+          src="/external/uncheckedcircle9512-ds9c-200h.png"
+          alt="UncheckedCircle9512"
+          className="group95-unchecked-circle"
+        /> */}
+      </div>
     </div>
   );
 };
