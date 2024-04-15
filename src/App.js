11526a226ce40d8904f1abeb77ee18a152397a20
LeeJungKyun <naeric7@naver.com>
2024-04-15 10:45:47 +0900

    [REFACTOR] 충돌파일 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { useRoutes } from "react-router-dom";
 import Themeroutes from "./routes/Router";
 
@@ -7,21 +6,5 @@ const App = () => {
 
   return <div className="dark">{routing}</div>;
 };
-=======
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginForm from "./pages/login";
-import SignUpForm from "./pages/singup";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="" element={<LoginForm></LoginForm>} />
-        <Route path="/signup" element={<SignUpForm></SignUpForm>} />
-      </Routes>
-    </Router>
-  );
-}
->>>>>>> Stashed changes
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,24 @@
-<<<<<<< Updated upstream
 import React, { Suspense } from "react";
 // import ReactDOM from "react-dom";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "../src/fonts/Fonts.css";
-import reportWebVitals from "./reportWebVitals";
-import App from "./App";
->>>>>>> Stashed changes
 
 root.render(
-<<<<<<< Updated upstream
   <Suspense fallback={<Loader />}>
     <HashRouter>
       <App />
     </HashRouter>
-  </Suspense>,
+  </Suspense>
 
   // document.getElementById("root")
-=======
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> Stashed changes
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
