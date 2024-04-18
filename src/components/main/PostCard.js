3cf2b6daa56db30a3b26515a6522d5e315f39b8e
yiyoungkim <lee20kim@gmail.com>
2024-04-18 18:23:37 +0900

    DTO-203: localmap 지도, 게시글 2차수정

--- src/components/main/PostCard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "../../assets/scss/layout/_postCard.scss";
-
-const NoticeCard = ({ children }) => {
-    return <div className="card">{children}</div>;
-};
-
-export default NoticeCard;
