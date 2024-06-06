16c8cbd5d4eca35daf88a32ce50a9362c6e9f52e
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2024-06-06 16:09:31 +0900

    [ADD] Footer 추가

--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Container } from "reactstrap";
+import "../assets/scss/layout/_fullLayout.scss"; // 스타일 추가
 
 const FullLayout = () => {
   return (
-    <main>
+    <main className="mainLayout">
       <div className="pageWrapper d-lg-flex">
         {/********Sidebar**********/}
         <aside className="sidebarArea shadow" id="sidebarArea">
@@ -23,9 +24,38 @@ const FullLayout = () => {
           {/********Footer**********/}
           <footer className="footer">
             <Container fluid className="text-center p-3">
-              <span>
-                © {new Date().getFullYear()} DTO. All rights reserved.
-              </span>
+              <div className="footer-content">
+                <div className="footer-section about">
+                  <h5>About Us</h5>
+                  <p>Gachon Univ. KEA 4기 DTO</p>
+                </div>
+                <div className="footer-section links">
+                  <h5>Links</h5>
+                  <ul>
+                    <li><a href="https://github.com/KEA4th-DTO" target="_blank">GitHub</a></li>
+                    <li><a href="https://www.notion.so/Develop-Together-Overnight-b4a5a74cfbd84dcf8e880deb57add202?pvs=4" target="_blank">About</a></li>
+                    <li><a href="#" target="_blank">Contact</a></li>
+                  </ul>
+                </div>
+                <div className="footer-section links">
+                  <h5>Follow Us</h5>
+                  <ul>
+                  <li><a href="#" target="_blank"><i></i>PM</a></li>
+                  <li><a href="#" target="_blank"><i></i>Architecture</a></li>
+                  <li><a href="#" target="_blank"><i></i>AppDev</a></li>
+                  </ul>
+                </div>
+                <div className="footer-section contact">
+                  <h5>Contact Us</h5>
+                  <p>Email: shc3113@gachon.ac.kr</p>
+                  <p>Phone: +82 010 8356 9899</p>
+                </div>
+              </div>
+              <div className="footer-bottom">
+                <span>
+                  © {new Date().getFullYear()} DTO. All rights reserved.
+                </span>
+              </div>
             </Container>
           </footer>
         </div>
